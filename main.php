927e44fe199ecb1798aaa09795d77ecec1c18c52
aligilan <ali.gilan.ag@gmail.com>
2020-11-01 09:33:08 +0330

    First commit, initialization

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+    namespace dompdf;
+
+    // reference the Dompdf namespace
+    use Dompdf\Dompdf;
+
+    // instantiate and use the dompdf class
+    $dompdf = new Dompdf();
+    // $content = readfile("./pages/html_page.html");
+    $content = file_get_contents("./pages/html_page.html");
+
+    $dompdf->loadHtml($content);
+
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'landscape');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Output the generated PDF to Browser
+    $dompdf->stream();
